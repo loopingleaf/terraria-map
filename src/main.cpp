2d60bf2ced691e80daa2125c233dab45b994546b
loopingleaf <luc.lauriou@live.fr>
2021-04-07 20:52:17 +0200

    Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include "Map.h"
 #include "SurfaceBuilder.h"
 #include "noise/noise.h"
 #include "noiseutils.h"
@@ -8,15 +9,42 @@ namespace ns = noise;
 
 int main()
 {
-	ns::module::Perlin perlin;
-	perlin.SetSeed(1564);
+	std::shared_ptr<ns::module::Perlin> perlin = std::make_shared<ns::module::Perlin>();
+	perlin->SetSeed(1456);
 	auto line = std::make_shared<ns::model::Line>();
-	line->SetModule(perlin);
-	line->SetStartPoint(0., 0., 0.);
-	line->SetEndPoint(1., 0., 0.);
-	SurfaceBuilder sb(line, 0.1);
-	Region region(25, 25);
-	sb.build(region, 0.);
+	SurfaceBuilder sb(perlin, 0.05);
+	
+	Map map(4, 1);
+	
+	/*sb.build(region, 0);
+	sb.build(region, 1);
+	sb.build(region, 2);
+	sb.build(region, 3);
+	sb.build(region, 4);
+	sb.build(region, 5);
+	sb.build(region, 6);
+	sb.build(region, 7);
+	sb.build(region, 8);
+	sb.build(region, 9);
+	sb.build(region, 10);*/
+
+	Region region1(4, 4);
+	Region region2(4, 4);
+	Region region3(4, 4);
+	Region region4(4, 4);
+	sb.build(region1, 0);
+	map.addRegion(region1, 0, 0);
+	sb.build(region2, 4);
+	map.addRegion(region2, 1, 0);
+	sb.build(region3, 8);
+	map.addRegion(region3, 2, 0);
+	sb.build(region4, 12);
+	map.addRegion(region4, 3, 0);
+	
+	/*sb.build(region, 4);
+	sb.build(region, 8);
+	sb.build(region, 12);*/
+	std::cout << map.toString() << std::endl;
 }
 
 /*int main()
